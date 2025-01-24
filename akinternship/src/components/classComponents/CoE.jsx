ded535e718aa97ec270af96f07468494046e8905
Ananth <ananthkishore.s2023sececse@gmail.com>
2025-01-24 19:20:41 +0530

    DAY 11 AN

---
+++ akinternship/src/components/classComponents/CoE.jsx
@@ -0,0 +1,12 @@
+
+import ExamResults from "./ExamResults"
+
+const Coe=()=>{
+    return (
+        <div>
+            <h2>this is COE</h2>
+            <ExamResults />
+        </div>
+    )
+}
+export default Coe
